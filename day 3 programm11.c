bdea081840aff8336e5bee15eac616cf01f093e6
laya18 <126039584+laya18@users.noreply.github.com>
2023-03-30 22:30:24 +0530

    Add files via upload

---
+++ day 3 programm11.c
@@ -0,0 +1,20 @@
+
+#include<stdio.h>
+void display(int);
+int main()
+{
+    int limit;
+    printf("Enter the number of terms to be printed\n");
+    scanf("%d", &limit);
+    printf("\nNatural Numbers from 1 To %d are:", limit);
+    display(limit);
+    return 0;
+}
+void display(int num)
+{
+    if(num)
+        display(num-1);
+    else
+    return 0;
+printf("\n%d\n", num);
+}
